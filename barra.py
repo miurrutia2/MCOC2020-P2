6f5aa25925ce050060105ed1041df3f040ac8235
jtcastellani <69157251+jtcastellani@users.noreply.github.com>
2020-10-10 11:51:52 -0300

    Add files via upload

--- barra.py
+++ barra.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 g = 9.81 #kg*m/s^2
 
 
@@ -18,104 +17,154 @@ class Barra(object):
 		self.σy = σy
 
 	def obtener_conectividad(self):
-		"""Implementar"""
 		return [self.ni, self.nj]
 
 	def calcular_area(self):
-		"""Implementar""" 
-		return (np.pi*self.R**2) - (np.pi*(self.R - self.t)**2)
+		A = np.pi*(self.R**2) - np.pi*((self.R-self.t)**2)
+		return A
 
 	def calcular_largo(self, reticulado):
 		"""Devuelve el largo de la barra. 
 		xi : Arreglo numpy de dimenson (3,) con coordenadas del nodo i
 		xj : Arreglo numpy de dimenson (3,) con coordenadas del nodo j
 		"""
-		"""Implementar""" 
 		xi = reticulado.obtener_coordenada_nodal(self.ni)
 		xj = reticulado.obtener_coordenada_nodal(self.nj)
-		dij = xi - xj
-		return np.sqrt(np.dot(dij, dij))
-
+		dij = xi-xj
+		return np.sqrt(np.dot(dij,dij))
 
 	def calcular_peso(self, reticulado):
 		"""Devuelve el largo de la barra. 
 		xi : Arreglo numpy de dimenson (3,) con coordenadas del nodo i
 		xj : Arreglo numpy de dimenson (3,) con coordenadas del nodo j
 		"""
-		"""Implementar"""
 		L = self.calcular_largo(reticulado)
 		A = self.calcular_area()
-		return A * L * g * self.ρ
+		return self.ρ * A * L * g
+
+
+
+
+
+
+
+
+
 
 
 	def obtener_rigidez(self, ret):
-		"""Devuelve la rigidez ke del elemento. Arreglo numpy de (4x4)
-		ret: instancia de objeto tipo reticulado
-		"""
-		
-		#implementar
-		L = self.calcular_largo(ret)
 		A = self.calcular_area()
-		k = self.E * A /L
-		
-		nix = ret.xyz[self.ni, 0]
-		niy = ret.xyz[self.ni, 1]
+		L = self.calcular_largo(ret)
 
-		njx = ret.xyz[self.nj, 0]
-		njy = ret.xyz[self.nj, 1]
+		xi = ret.obtener_coordenada_nodal(self.ni)
+		xj = ret.obtener_coordenada_nodal(self.nj)
 
-		cos = (njx - nix) / L
-		sen = (njy - niy) / L
+		cosθx = (xj[0] - xi[0])/L
+		cosθy = (xj[1] - xi[1])/L
+		cosθz = (xj[2] - xi[2])/L
 
-		Ttheta = np.array([-cos, -sen, cos, sen])
-		ke = k * Ttheta.T @ Ttheta 
+		Tθ = np.array([ -cosθx, -cosθy, -cosθz, cosθx, cosθy, cosθz ]).reshape((6,1))
 
-		return ke
+		return self.E * A / L * (Tθ @ Tθ.T )
 
 	def obtener_vector_de_cargas(self, ret):
-		"""Devuelve el vector de cargas nodales fe del elemento. Vector numpy de (4x1)
-		ret: instancia de objeto tipo reticulado
-		"""
-		
-		#Implementar
-
 		W = self.calcular_peso(ret)
-		fe = np.array([0,-1,0,-1]).T*W/2
 
-		return fe
+		return np.array([0, 0, -W, 0, 0, -W])
 
 
 	def obtener_fuerza(self, ret):
-		"""Devuelve la fuerza se que debe resistir la barra. Un escalar tipo double. 
-		ret: instancia de objeto tipo reticulado
+		ue = np.zeros(6)
+		ue[0:3] = ret.obtener_desplazamiento_nodal(self.ni)
+		ue[3:] = ret.obtener_desplazamiento_nodal(self.nj)
+		
+		A = self.calcular_area()
+		L = self.calcular_largo(ret)
+
+		xi = ret.obtener_coordenada_nodal(self.ni)
+		xj = ret.obtener_coordenada_nodal(self.nj)
+
+		cosθx = (xj[0] - xi[0])/L
+		cosθy = (xj[1] - xi[1])/L
+		cosθz = (xj[2] - xi[2])/L
+
+		Tθ = np.array([ -cosθx, -cosθy, -cosθz, cosθx, cosθy, cosθz ]).reshape((6,1))
+
+		return self.E * A / L * (Tθ.T @ ue)
+
+
+
+
+
+	def chequear_diseño(self, Fu, ϕ=0.9):
+		"""Para la fuerza Fu (proveniente de una combinacion de cargas)
+		revisar si esta barra cumple las disposiciones de diseño.
+		"""
+		A = self.calcular_area()
+		Fn = A*self.σy
+
+		if ϕ*Fn < abs(Fu):
+			return False
+		else:
+			return True
+
+
+	def obtener_factor_utilizacion(self, Fu, ϕ=0.9):
+		"""Para la fuerza Fu (proveniente de una combinacion de cargas)
+		calcular y devolver el factor de utilización
+		"""
+		A = self.calcular_area()
+		Fn = A*self.σy 
+
+		return abs(Fu) / abs(ϕ*Fn)
+
+
+	def rediseñar(self, Fu, ret, ϕ=0.9):
+		"""Para la fuerza Fu (proveniente de una combinacion de cargas)
+		re-calcular el radio y el espesor de la barra de modo que
+		se cumplan las disposiciones de diseño lo más cerca posible
+		a FU = 1.0.
 		"""
 
-		#Implementar
-		L = self.calcular_largo(ret)
-		A = self.calcular_area()
+		FdU = self.obtener_factor_utilizacion(Fu, ϕ)
+		Asolicitada = abs(Fu) / (self.σy * ϕ)
+		Adiseno = self.calcular_area()
+		Re = self.R
+		Ri = Re - self.t
 
-		nix = ret.xyz[self.ni, 0]
-		niy = ret.xyz[self.ni, 1]
+		def FunA(R, t):
+			return np.pi * (R**2 - (R - t)**2)
 
-		njx = ret.xyz[self.nj, 0]
-		njy = ret.xyz[self.nj, 1]
+		def Funt(R, A):
+			return int(R - np.sqrt((np.pi * R**2 - A) / np.pi))
 
-		cos = (njx - nix) / L
-		sen = (njy - niy) / L
+		if Adiseno < Asolicitada:
+			self.R = Re + 1
 
-		Ttheta = np.array([-cos, -sen, cos, sen])
+			if self.chequear_diseño:
+				self.t = Funt(self.R, self.calcular_area())
 
-		ue = np.array([ret.u[self.ni*2], ret.u[((2*self.ni)+1)], ret.u[2*self.nj], ret.u[((2*self.nj)+1)]])
+			if not self.chequear_diseño:
+				self.rediseñar(self, Fu, ϕ, ret)
 
-		se = (self.E * A / L) * Ttheta.T @ ue
+		if Adiseno == Asolicitada:
+			return None
+
+		if Adiseno > Asolicitada:
+			self.R = Re -1
+
+			if self.chequear_diseño:
+				self.t = Funt(self.R, self.calcular_area())
+
+			if not self.chequear_diseño:
+				self.rediseñar(self, Fu, ϕ, ret)
 
 
-		return se
 
 
 
-
-
-
+		#self.R = 0.9*self.R   #cambiar y poner logica de diseño
+		#self.t = 0.9*self.t   #cambiar y poner logica de diseño
+		return None
 
 
